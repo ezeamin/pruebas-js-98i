59a117ce3d0fc2a4e0bbb07976e1b3faa571def2
Ezequiel Amin <ezequielamin@outlook.com>
2024-03-14 15:55:31 -0300

    primer commit de los ejercicios de js

---
+++ js/app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+console.log('Hola mundo!');
+
+let nombre = 'Ezequiel';
+let miNumero = 6;
+
+// console.log(nombre);
+// console.log(miNumero);
+
+// miNumero = 'lol xd';
+
+// console.lg(miNumero);
+
+// int miNumero = 6;
+
+// let nombre;
+// nombre = 'Eze';
+
+// const nombre = 'Eze';
+// console.log(nombre);
+
+// nombre = 'Gabi';
+// console.log(nombre);
+
+// alert('Este es un texto en un alert, bienvenido ' + nombre);
+
+// const respuesta = prompt('Ingrese su nombre');
+// console.log(respuesta, typeof respuesta);
+
+// OPERADORES RELACIONALES
+
+// >, <, >=, <=, ==, ===, !=, !==
+
+// console.log(2 > 5)
+// console.log(2 < 5)
+// console.log(2 >= 5)
+// console.log(2 <= 5)
+// console.log(5 >= 5)
+// console.log(2 == 5)
+// console.log(5 === 5)
+// console.log(5 == '5')
+// console.log(5 === '5')
+// console.log(5 != '5')
+// console.log(5 !== '5')
+
+// SI esto se cumple (es true)
+if (2 > 10) {
+  // se ejecuta esto
+  console.log('Se ejecutó esto');
+} else {
+  // SINO
+  // se ejecuta esto cuando lo de arriba no
+  console.log('Llegamos al else');
+}
+
+if (2 > 10) {
+  // se ejecuta esto
+  console.log('Se ejecutó esto');
+} else if (15 > 10) {
+  // SINO
+  // se ejecuta esto cuando lo de arriba no
+  console.log('Llegamos al else if');
+} else {
+  console.log('Llegamos al último else');
+}
+
+document.write()
