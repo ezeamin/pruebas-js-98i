5b54e674096ac52334ba3c8863cb3d11021ab4d9
Ezequiel Amin <ezequielamin@outlook.com>
2024-03-19 16:30:01 -0300

    agrego enunciado de ejercicio 2

---
+++ js/tp2/ejercicio2.js
@@ -0,0 +1,27 @@
+/*
+Escribir un programa que solicite una nota (número) de 0 a 10. 
+Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+
+0-2: Muy deficiente
+3-4: Insuficiente
+5-6: Suficiente
+7: Bien
+8-9: Notable
+10: Sobresaliente
+
+Si ingreso un número que no esté dentro del rango de 0 a 10
+mostrar un mensaje de “número erróneo”. Si el número ingresado
+no es válido mostrar el mensaje “Introduce un número válido”.
+
+Ejemplo:
+
+Input: 5 -> Output: Suficiente
+Input: 50 -> Output: Número erróneo
+Input: hola10 -> Output: Introduce un número válido
+
+1. Pedir un número
+2. Validar si es un número
+3. Validar el rango (0-10)
+4. Verificar resultado de calificación 
+5. Mostrar el mensaje
+*/
