cde0ca5d2a5ec784e7f062f11fe60cfa74fd1754
Ezequiel Amin <ezequielamin@outlook.com>
2024-04-11 15:42:43 -0300

    agrego objetos.js y clases.js

---
+++ js/clases.js
@@ -0,0 +1,29 @@
+// PascalCase
+class Persona {
+  // Propiedades usando el constructor
+  constructor(nombre, edad, estaCasado) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.estaCasado = estaCasado;
+  }
+
+  // Métodos
+  presentarse() {
+    console.log(`Hola! Soy ${this.nombre}, tengo ${this.edad} años`);
+  }
+
+  crecer() {
+    this.edad++;
+    console.log(`Crecí! Tengo ${this.edad} años`);
+  }
+}
+
+const juan = new Persona('juan', 22, true);
+const lucia = new Persona('lucia', 33, false);
+
+console.log(juan);
+console.log(lucia);
+
+juan.presentarse();
+lucia.presentarse();
+
---
+++ js/objetos.js
@@ -0,0 +1,36 @@
+const persona = {
+  // Propiedades
+  nombre: 'Pepe',
+  edad: 22,
+  estaCasado: true,
+  idiomas: ['Español', 'Inglés'],
+  direccion: {
+    calle: 'Av. Aconquija',
+    numero: '123',
+    ciudad: 'Yerba Buena',
+    pais: 'Argentina',
+  },
+
+  // Metodos
+  presentarse: function () {
+    console.log(`Hola! Soy ${this.nombre}, tengo ${this.edad} años`);
+  },
+  crecer: function () {
+    this.edad++;
+    console.log(`Crecí! Tengo ${this.edad} años`);
+  },
+};
+
+// Hoisting
+
+console.log(persona);
+persona.presentarse();
+persona.crecer();
+
+persona.apellido = 'Juarez';
+persona.edad = 15;
+
+console.log(persona);
+
+const nombrePropiedad = 'nombre';
+console.log(persona[nombrePropiedad]);
