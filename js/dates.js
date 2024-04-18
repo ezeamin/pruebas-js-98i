156a5dd3ff2b695874e97e790cb44ae754f657e4
Ezequiel Amin <ezequielamin@outlook.com>
2024-04-18 16:22:17 -0300

    agrego fechas

---
+++ js/dates.js
@@ -0,0 +1,19 @@
+const fecha = new Date('2025-2-1');
+console.log(fecha);
+
+console.log(fecha.toISOString());
+
+console.log(fecha.getFullYear());
+console.log(fecha.getMonth());
+console.log(fecha.getDate());
+console.log(fecha.getDay());
+console.log(fecha.getHours());
+console.log(fecha.getMinutes());
+console.log(fecha.getSeconds());
+
+console.log(fecha.toLocaleDateString())
+console.log(fecha.toLocaleTimeString())
+console.log(fecha.toLocaleString())
+console.log(fecha.toDateString())
+console.log(fecha.toString())
+
